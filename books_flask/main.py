3c19fc2cb9784f0d7bef98665796d61b8ff18663
root <root@Hermes.localdomain>
2020-12-13 00:12:13 +0800

    rm -rf old env and create new env in books_flask.

---
+++ books_flask/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding=UTF-8
+'''
+Author: dofospider
+since: 2020-12-13 00:07:24
+lastTime: 2020-12-13 00:07:24
+LastAuthor: Do not edit
+'''
---
+++ books_flask/settings.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding=UTF-8
+'''
+Author: dofospider
+since: 2020-12-13 00:07:39
+lastTime: 2020-12-13 00:07:39
+LastAuthor: Do not edit
+'''
