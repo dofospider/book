7ddbf399e70b3a2800f6875e79a5c67182df5580
dofospider <dofospider@gmail.com>
2020-12-16 17:12:33 +0800

    empty project,ready for start

--- books_vue/books/src/router/index.js
+++ books_vue/books/src/router/index.js
@@ -1,3 +1,9 @@
+/*
+ * @Author: dofospider
+ * @since: 2020-12-16 14:49:16
+ * @lastTime: 2020-12-16 17:08:23
+ * @LastAuthor: Do not edit
+ */
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
@@ -10,15 +16,15 @@ const routes = [
     name: "Home",
     component: Home
   },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+  // {
+  //   path: "/about",
+  //   name: "About",
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ "../views/About.vue")
+  // }
 ];
 
 const router = new VueRouter({
--- books_vue/books/src/views/Home.vue
+++ books_vue/books/src/views/Home.vue
@@ -1,18 +1,22 @@
+<!--
+ * @Author: dofospider
+ * @since: 2020-12-16 14:49:16
+ * @lastTime: 2020-12-16 17:08:33
+ * @LastAuthor: Do not edit
+-->
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+   
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+  
   }
 };
 </script>
